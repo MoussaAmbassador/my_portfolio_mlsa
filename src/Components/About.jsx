f48f3254e04112c7469c22ffe1d2097a14ea1d02
MoussaAmbassador <157999756+MoussaAmbassador@users.noreply.github.com>
2024-02-05 08:59:37 +0000

    Adding skills

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,28 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Microsoft Learn Student Ambassador, Cloud Engineer & Electrical Engineering student at Ecole Nationale d'Ingenieurs Abderhamane Baba Toure from Bamako, Mali. I enjoy building robust, secure and scalable Cloud application. Google Developer Student Lead Alumni, I am community driven and I enjoy helping others get the best out of themselves. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "Cloud Computing",
+  "System Administration",
+  "Google Cloud",
+  "Azure",
+  "Linux",
+  "Bash",
+  "SQL",
+  "Docker",
+  "Kubernetes",
+  "Terraform",
+  "Office 365",
+  "Machine Learning",
+  "Systems Designs"
+
 ];
 
 /**
@@ -45,7 +54,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Cloud & Systems Designs experience I continually look for new and better ways to build and make technology solutions accessible by all.";
 
 const About = () => {
   return (
